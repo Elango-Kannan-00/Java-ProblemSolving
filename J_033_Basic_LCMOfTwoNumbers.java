b9f5443c16ed7fb26351eca67d695b5b12cae201
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-08-26 14:31:14 +0530

    Added, finding LCM of 2 numbers.

---
+++ J_033_Basic_LCMOfTwoNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class J_033_Basic_LCMOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number one: ");
+        int one = scan.nextInt();
+        int originalOne = one;
+
+        System.out.print("Enter number two: ");
+        int two = scan.nextInt();
+        int originalTwo = two;
+        
+        if (one > two){ 
+            while (two > 0){
+                int digit = one % two;
+                one = two; two = digit;
+            }
+            int lcm = (originalOne * originalTwo)/one;
+            System.out.println("LCM is: " + lcm);
+        }
+        else {
+             while (one > 0){
+                int digit = two % one;
+                two = one; one = digit;
+            }
+            int lcm = (originalOne * originalTwo)/two;
+            System.out.println("LCM is: " + lcm);
+        }
+        scan.close();
+    }
+}
