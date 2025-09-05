779b6fe7812409d64e748cc0c201f118682dd259
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-05 22:33:50 +0530

    Refactored character counting logic to improve clarity and removed unused string conversion code.

--- J_073_Basic_CountUpperLowercase.java
+++ J_073_Basic_CountUpperLowercase.java
@@ -6,12 +6,22 @@ public class J_073_Basic_CountUpperLowercase {
         System.out.println("Enter any string: ");
         String word = scan.nextLine();
 
-        StringBuilder newWord = new StringBuilder();
-        for (int i=0; i<word.length(); i++){ 
-            if (Character.isLowerCase(word.charAt(i))) { newWord.append(Character.toUpperCase(word.charAt(i))); }
-            else { newWord.append(Character.toLowerCase(word.charAt(i))); }
+        int lower = 0, upper = 0;
+        for (int i=0; i<word.length(); i++) { 
+            if (word.charAt(i) != ' '){ 
+                if (Character.isUpperCase(word.charAt(i))) { upper++; }
+                else { lower++; }
+            }
         }
-        System.out.println("Updated string is: " + newWord);
+        System.out.println("Number of upper and lower case characters are " + upper + " and " + lower + " respectively.");
         scan.close();
+
+        // Converting lower to uppercase and upper to lowercase characters.
+        // StringBuilder newWord = new StringBuilder();
+        // for (int i=0; i<word.length(); i++){ 
+        //     if (Character.isLowerCase(word.charAt(i))) { newWord.append(Character.toUpperCase(word.charAt(i))); }
+        //     else { newWord.append(Character.toLowerCase(word.charAt(i))); }
+        // }
+        // System.out.println("Updated string is: " + newWord);
     }
 }
