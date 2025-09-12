d1a62cf9ea4efd8bc4d5ba77b0b65af6b61ee45d
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-12 21:58:27 +0530

    Refactored sum calculation logic for first n even numbers and updated output message.

--- J_085_Basic_SumFirstNEvenNumbers.java
+++ J_085_Basic_SumFirstNEvenNumbers.java
@@ -5,20 +5,20 @@ public class J_085_Basic_SumFirstNEvenNumbers {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the range value: ");
         int range = scan.nextInt();
-        // int sumValue = 0, count = 0, a = 1;
+        int sumValue = 0, count = 0, a = 1;
 
-        // if (range < 0) { System.out.println("Please enter valid range."); }
-        // else {
-        //     while (count < range) {  if (a % 2 == 0) { sumValue += a; count++; } a++;  }
-        // }
-        // System.out.println("The sum of first " + range + " odd numbers is: " + sumValue);
-        // scan.close();
+        if (range < 0) { System.out.println("Please enter valid range."); }
+        else {
+            while (count < range) {  if (a % 2 == 0) { sumValue += a; count++; } a++;  }
+        }
+        System.out.println("The sum of first " + range + " odd numbers is: " + sumValue);
+        scan.close();
 
         // other methods using formulas.
         // For finding the sum of first n odd numbers.
-        System.out.println("The sum of first " + range + " odd numbers is: " + (range * range));
+        // System.out.println("The sum of first " + range + " odd numbers is: " + (range * range));
 
         // For finding the sum of first n even nmumbers.
-        System.out.println("The sum of forst " + range + " even numbers is: " + (range*(range + 1)));
+        // System.out.println("The sum of forst " + range + " even numbers is: " + (range*(range + 1)));
     }
 }
