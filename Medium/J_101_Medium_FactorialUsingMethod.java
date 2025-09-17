5f3b3460b7b617a23e26c1c7ab01e8ed2bc8413e
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-17 23:13:47 +0530

    Added, finding factorial of a number using method.

---
+++ Medium/J_101_Medium_FactorialUsingMethod.java
@@ -0,0 +1,20 @@
+package Medium;
+
+import java.util.Scanner;
+
+public class J_101_Medium_FactorialUsingMethod {
+    public static int factorial(int n){
+        int productValue = 1;
+        for (int i=n; i>0; i--) { productValue *= i; }
+        return productValue;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int number = scan.nextInt();
+
+        System.out.println("The factorial of a given number " + number + " is: " + factorial(number));
+        scan.close();
+    }
+}
