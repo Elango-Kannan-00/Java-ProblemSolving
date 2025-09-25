0dfc5753b743027a09dd60b4762f8cf320a13fbe
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-25 23:04:53 +0530

    Added, Removing duplicate elements in an array.

--- Basic/J_053_Basic_RemoveDuplicatesFromArray.java
+++ Basic/J_053_Basic_RemoveDuplicatesFromArray.java
@@ -7,19 +7,26 @@ public class J_053_Basic_RemoveDuplicatesFromArray {
         int size = scan.nextInt();
 
         int[] numArray = new int[size];
-        for (int i=0; i<size; i++) { numArray[i] = scan.nextInt(); }
+        for (int i = 0; i < size; i++) {
+            numArray[i] = scan.nextInt();
+        }
 
         int[] uniqueArray = new int[size];
         boolean[] freqArray = new boolean[size];
         int uniqueIndex = 0;
-        for (int i=0; i<size; i++) { 
-            if (freqArray[i]) continue; 
-            for (int j=i+1; j<size; j++){
-                if (numArray[i] == numArray[j]) { freqArray[j] = true; }
+        for (int i = 0; i < size; i++) {
+            if (freqArray[i])
+                continue;
+            for (int j = i + 1; j < size; j++) {
+                if (numArray[i] == numArray[j]) {
+                    freqArray[j] = true;
+                }
             }
             uniqueArray[uniqueIndex++] = numArray[i];
         }
-        for (int i=0; i<uniqueIndex; i++) { System.out.print(uniqueArray[i] + " "); }
+        for (int i = 0; i < uniqueIndex; i++) {
+            System.out.print(uniqueArray[i] + " ");
+        }
         scan.close();
     }
 }
