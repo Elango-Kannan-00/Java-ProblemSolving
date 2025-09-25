8a200400218cdca5e8ca423f83f8a6c250cef869
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-25 09:37:32 +0530

    Added, Printing number pattern.

--- Basic/J_102_Basic_NumberPattern.java
+++ Basic/J_102_Basic_NumberPattern.java
@@ -8,7 +8,7 @@ public class J_102_Basic_NumberPattern {
 
         for (int i = 1; i <= range; i++) {
             for (int j = 1; j <= range - i; j++) {
-                System.out.print("  ");
+                System.out.print("  "); 
             }
             int a = i;
             for (int k = 1; k <= (i * 2) - 1; k++) {
@@ -34,3 +34,36 @@ public class J_102_Basic_NumberPattern {
 //     3 4 5 4 3 
 //   4 5 6 7 6 5 4 
 // 5 6 7 8 9 8 7 6 5 
+
+// Optimised version:
+// import java.util.Scanner;
+
+// public class J_102_Basic_NumberPattern {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         System.out.print("Enter number of rows: ");
+//         int range = scan.nextInt();
+
+//         for (int i = 1; i <= range; i++) {
+//             // Print leading spaces
+//             for (int j = 0; j < range - i; j++) {
+//                 System.out.print("  ");
+//             }
+
+//             // Print number pattern
+//             int num = i;
+//             for (int k = 1; k <= (2 * i - 1); k++) {
+//                 System.out.print(num + " ");
+//                 if (k < i) {
+//                     num++;
+//                 } else {
+//                     num--;
+//                 }
+//             }
+
+//             System.out.println();
+//         }
+
+//         scan.close();
+//     }
+// }
