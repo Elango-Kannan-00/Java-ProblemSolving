689fd292c81297cc45c9b6a331aa3194eb126ec4
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-09-29 23:51:25 +0530

    Added, Finding LCM of a number using Method.

---
+++ Medium/J_109_Medium_GCDOfTwoNumbersUsingMethod.java
@@ -0,0 +1,36 @@
+package Medium;
+import java.util.Scanner;
+
+public class J_109_Medium_GCDOfTwoNumbersUsingMethod {
+    public static int lcmCalculation(int n1, int n2) {
+        int a = n1, b = n2;
+        if (n1 > n2) {
+            while (n2 > 0) {
+                int digit = n1 % n2;
+                n1 = n2;
+                n2 = digit;
+            }
+            int lcm = (a * b) / n1;
+            return lcm;
+        } else {
+            while (n1 > 0) {
+                int digit = n2 % n1;
+                n2 = n1;
+                n1 = digit;
+            }
+            int lcm = (a * b) / n2;
+            return lcm;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int number1 = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = scan.nextInt();
+
+        System.out.println("LCM of two numbers is: " + lcmCalculation(number1, number2));
+        scan.close();
+    }    
+}
